80a69effe2eb1856a97c64efdfd66f1e0707e9b3
GIT-DCOLLAO <102884636+git-dcollao@users.noreply.github.com>
2022-08-22 08:31:31 -0400

    Update settings.py

--- ProyectoCode31095/settings.py
+++ ProyectoCode31095/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qlsh-o+8#3vzr-p4fvgha-aezuy#v7xe7=7^b40!)+lo3tf(%6"
+SECRET_KEY = "django----------------------------"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
